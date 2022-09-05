7340e47cfd5f7c3aa5ec205f506c629a601a509f
Joshua Bugryn <46905032+bugryn-josh@users.noreply.github.com>
2022-09-05 19:28:33 -0400

    Update: started writing multi-user challenge functionality

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const express = require("express");
+const bunyan = require("bunyan");
 const session = require("express-session");
 const nunjucks = require('nunjucks')
 const config = require('./config');
 const duo_api = require('@duosecurity/duo_api');
 
 class AuthFlow {
+    #res;
+    #client;
     constructor(client, username, res){
         this.username = username;
-        this.res = res;
+        this.#res = res;
         this.currData = null;
         this.state = 'preAuth';
-        this.client = client;
+        this.#client = client;
     }
 
-    updateFlow = (data, state, callback) => {
+    #updateFlow = (data, state, callback) => {
         this.currData = data;
         this.state = state;
         callback();
@@ -23,15 +26,59 @@ class AuthFlow {
         this.client.jsonApiCall('POST', '/auth/v2/preauth', {username : this.username}, (data) => {this.updateFlow(data, 'sendPush', this.sendPush)});
     }
 
-    sendPush = () => {
+    #sendPush = () => {
         const devices = this.currData.response?.devices[0]
 
         if (devices.capabilities.indexOf('push') !== -1 && this.currData?.stat === 'OK'){
-            this.client.jsonApiCall('POST', '/auth/v2/auth', {username: this.username, factor: 'auto', device: 'auto'}, (data) => {this.updateFlow(data, 'awaitPush', this.procResult)});
+            this.client.jsonApiCall('POST', '/auth/v2/auth', {username: this.username, factor: 'auto', device: 'auto', type : 'Remote security challenge', pushinfo: 'from=Demo%20Challenge&domain=test.com&reason=approval%20for%20action'}, (data) => {this.updateFlow(data, 'awaitPush', this.procResult)});
     }
 }
 
-    procResult = () => {
+    #procResult = () => {
+        if (this.currData?.response?.result === 'allow' && this.state === 'awaitPush') {
+            this.res.render('success.html', {message: 'Challenge Approved'});
+        } else {
+            this.res.render('success.html', {message: 'Challenge Denied'});
+        }
+    }
+}
+
+class MultiAuthFlow {
+    #res;
+    #client;
+    constructor(client, usernames, threshold, res){
+        this.usernames = usernames;
+        this.#res = res;
+        this.currData = null;
+        this.state = this.#initMultiState(usernames);
+        this.#client = client;
+        this.threshold = threshold;
+        this.approved = [];
+    }
+
+    #initMultiState = (usernames) => {
+        return usernames.reduce((obj, key) => (obj[key]='preAuth', obj), {});
+    }
+
+    #updateFlow = (data, state, callback) => {
+        this.currData = data;
+        this.state = state;
+        callback();
+    }
+
+    startAuth = () => {
+        this.client.jsonApiCall('POST', '/auth/v2/preauth', {username : this.username}, (data) => {this.updateFlow(data, 'sendPush', this.sendPush)});
+    }
+
+    #sendPush = () => {
+        const devices = this.currData.response?.devices[0]
+
+        if (devices.capabilities.indexOf('push') !== -1 && this.currData?.stat === 'OK'){
+            this.client.jsonApiCall('POST', '/auth/v2/auth', {username: this.username, factor: 'auto', device: 'auto', type : 'Remote security challenge', pushinfo: 'from=Demo%20Challenge&domain=test.com&reason=approval%20for%20action'}, (data) => {this.updateFlow(data, 'awaitPush', this.procResult)});
+    }
+}
+
+    #procResult = () => {
         if (this.currData?.response?.result === 'allow' && this.state === 'awaitPush') {
             this.res.render('success.html', {message: 'Challenge Approved'});
         } else {
@@ -65,23 +112,51 @@ const startApp = async () => {
         return;
     });
 
-    app.post('/', async (req, res) => {
-        const {username,password} = req.body;
+    app.get('/multi', async(req, res) => {
+        res.render('indexMulti.html', {message: ''});
+    });
 
-        if (!username || !password) {
+    app.post('/', async (req, res) => {
+        const {username} = req.body;
+
+
+        if (!username) {
             res.render('index.html', {message: 'Missing username or password'});
         return;
         }
 
         try {
-
+                
             flow = new AuthFlow(client, username, res);
-
             flow.startAuth()
+        
+            } catch (err) {
+                console.log(err);
+        }
+    });
 
+    app.post('/multi', async(req, res) => {
+        const {usernames, threshold} = req.body;
+
+        console.log(usernames)
+
+
+        if (!usernames || !Array.isArray(usernames)) {
+            res.render('indexMulti.html', {message: 'Missing username or single user provided.'});
+        return;
+        }
+
+        if (!threshold || threshold > usernames.length) {
+            res.render('indexMulti.html' , {message: 'Threshold missing or greater than number of users.'});
+        }
+
+        try {
+            flow = new MultiAuthFlow(client, usernames, threshold, res);
+            console.log(flow)
         } catch (err) {
             console.log(err);
         }
+
     });
 
     // Start listening
